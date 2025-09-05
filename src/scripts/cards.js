eaf29f619cf3636bb9d90e96ae99772644002662
Pedro <pedrolimanunes2011@gmail.com>
2025-09-05 18:54:20 -0300

    Elaboração dos métodos das cartas

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -14,9 +14,31 @@ class CardManager {
         let template = document.getElementById("cardTemplate"); // Referenciar o template
         let clone = template.content.cloneNode(true);           // Criar um clone
 
+        // Capturar os cliques nas cartas
+        clone.children[0].addEventListener("click",
+            event => this.onClick(event)
+        )
+
         // Retornar o clone
         return clone;
     }
+// Capture eventos de clique
+    onClick(event) {
+        this.flip(event.target)
+    }
+// Vira a carta
+    flip(cardNode) {
+        cardNode.children[0].classList.add("selected");
+    }
+// Desvira a carta
+    unFlip(cardNode) {
+        cardNode.children[0].classList.remove("selected");
+    }
+// Método que desabilita o movimento da carta que já foi virada
+    disable(cardNode) {
+        cardNode.children[0].classList.remove("matched");
+        this.unFlip(cardNode);
+    }
 
-    
+
 }
